1f37ad842676263adb34b8b92d3adf5f166e5869
IAGO BATISTA DE LIMA <72325459+IagoB7ima@users.noreply.github.com>
2021-08-28 12:32:32 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Item from './components/Item';
+import Card from './components/Card';
+
+const App = () =>{
+  return(
+    <>
+      <h1>Minha primeira aplicação com react</h1>
+      <ul>
+        <Item>
+          Item1
+        </Item>
+        <Item>
+          Item2
+        </Item>
+        <Item>
+          Item3
+        </Item>
+      </ul>
+      <Card/>
+    </>
+      )
+}
+
+export default App;
